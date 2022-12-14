d9ceb159f4dfde44b29e3beb622fc1649becc068
mattg-97 <mattjohngill@gmail.com>
2022-12-14 12:01:12 +0000

    add project endpoint finalised

--- src/controllers/ProjectsController.js
+++ src/controllers/ProjectsController.js
@@ -3,9 +3,6 @@ import { CreateNewProject, UpdateContributors } from '../../db/dataManager';
 export const AddProject = async (req, res) => {
   try {
     const { project: projectName, livesBetter: totalUsers, users: contributors } = req.body;
-    console.log(projectName);
-    console.log(totalUsers);
-    console.log(contributors);
     await CreateNewProject(projectName, totalUsers);
     await UpdateContributors(projectName, contributors);
     res.send(`Project ${projectName} created.`);
