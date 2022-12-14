0f288cb91e204d1a256e0ad6b5e78105a5ce076c
mattg-97 <mattjohngill@gmail.com>
2022-12-14 13:58:39 +0000

    changes add dporject to accept string parameter for livesBetter

--- src/controllers/ProjectsController.js
+++ src/controllers/ProjectsController.js
@@ -2,8 +2,8 @@ import { CreateNewProject, UpdateContributors } from '../../db/dataManager';
 
 export const AddProject = async (req, res) => {
   try {
-    const { project: projectName, livesBetter: totalUsers, users: contributors } = req.body;
-    await CreateNewProject(projectName, totalUsers);
+    const { projectName, livesBetter: totalUsers, users: contributors } = req.body;
+    await CreateNewProject(projectName, Number(totalUsers));
     await UpdateContributors(projectName, contributors);
     res.send(`Project ${projectName} created.`);
   } catch (error) {
