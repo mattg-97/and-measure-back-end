6414c5b6a7e62a79cf1b0f2629a30dff5b67e49a
mattg-97 <mattjohngill@gmail.com>
2022-12-14 09:47:56 +0000

    changed individual contribution to use query params

--- src/controllers/ContributionController.js
+++ src/controllers/ContributionController.js
@@ -4,7 +4,7 @@ const pool = require('../../db/db');
 
 export const GetIndividualContribution = async (req, res) => {
   try {
-    const { user_id: userId } = req.body;
+    const userId = Number(req.query.user_id);
     const indiviudualContribution = await pool.query('select first_name, last_name, and_title, project_id, project_name, total_users from users inner join (select projects.project_id, project_name, total_users, user_id from projects inner join (select * from andi_contribution_log where user_id = $1) as contributions ON projects.project_id = contributions.project_id) as t1 on users.user_id = t1.user_id;', [userId]);
     res.json(await CreateIndiviudalResponseObject(indiviudualContribution));
   } catch (error) {
@@ -24,8 +24,13 @@ export const GetClubContribution = async (req, res) => {
 
 export const GetANDContribution = async (req, res) => {
   try {
-    const andContribution = await pool.query('');
-    res.json(andContribution);
+    const andContribution = await pool.query('Select DISTINCT project_name, total_users, club_name, club_id from (select * from projects p inner join(Select * from andi_contribution_log a inner join (Select c.club_id, club_name, user_id from clubs c inner join (Select * from users) u on c.club_id = u.club_id) b on a.user_id = b.user_id) c on p.project_id = c.project_id) as x ORDER BY club_name;');
+
+    for (const contribution of andContribution.rows) {
+      console.log(contribution);
+    }
+
+    res.json(andContribution.rows);
   } catch (error) {
     res.send(error);
   }
