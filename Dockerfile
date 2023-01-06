388682c28c90360eb5b524816005314958e6d4db
mattg-97 <mattjohngill@gmail.com>
2023-01-06 09:24:22 +0000

    React learning safety commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:12
+
+WORKDIR .
+
+COPY package.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 8080
+
+CMD ["npm", "start"]
+
--- src/controllers/ContributionController.js
+++ src/controllers/ContributionController.js
@@ -1,10 +1,17 @@
-import { CreateANDContributionObject, CreateClubResponseObject, CreateIndiviudalResponseObject, HandleClubContributionRequest } from '../../db/dataManager';
+import {
+  CreateANDContributionObject,
+  CreateClubResponseObject,
+  CreateIndiviudalResponseObject,
+  HandleClubContributionRequest
+} from '../../db/dataManager';
 
 const pool = require('../../db/db');
 
 export const GetANDContribution = async (req, res) => {
   try {
-    const andContribution = await pool.query('Select club_name as clubName, sum(total_users) as totalUsers from (select * from projects p inner join(Select * from andi_contribution_log a inner join (Select c.club_id, club_name, user_id from clubs c inner join (Select * from users) u on c.club_id = u.club_id) b on a.user_id = b.user_id) c on p.project_id = c.project_id) as x GROUP BY club_name;');
+    const andContribution = await pool.query(
+      'Select club_name as clubName, sum(total_users) as totalUsers from (select * from projects p inner join(Select * from andi_contribution_log a inner join (Select c.club_id, club_name, user_id from clubs c inner join (Select * from users) u on c.club_id = u.club_id) b on a.user_id = b.user_id) c on p.project_id = c.project_id) as x GROUP BY club_name;'
+    );
     const andContributionObject = await CreateANDContributionObject(andContribution.rows);
     res.json(andContributionObject);
   } catch (error) {
@@ -35,10 +42,15 @@ export const GetGlobalProgress = async (req, res) => {
   }
 };
 
+export const myFunc = () => {};
+
 export const GetIndividualContribution = async (req, res) => {
   try {
     const userId = Number(req.query.userId);
-    const indiviudualContribution = await pool.query('select first_name, last_name, and_title, club_id, project_id, project_name, total_users from users inner join (select projects.project_id, project_name, total_users, user_id from projects inner join (select * from andi_contribution_log where user_id = $1) as contributions ON projects.project_id = contributions.project_id) as t1 on users.user_id = t1.user_id;', [userId]);
+    const indiviudualContribution = await pool.query(
+      'select first_name, last_name, and_title, club_id, project_id, project_name, total_users from users inner join (select projects.project_id, project_name, total_users, user_id from projects inner join (select * from andi_contribution_log where user_id = $1) as contributions ON projects.project_id = contributions.project_id) as t1 on users.user_id = t1.user_id;',
+      [userId]
+    );
     res.json(await CreateIndiviudalResponseObject(indiviudualContribution));
   } catch (error) {
     res.send(error);
